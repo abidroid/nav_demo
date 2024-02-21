27573931ea34279087bd20c48dfe25650101e4d8
Muhammad Abid <abidroid1@gmail.com>
2024-02-21 09:47:00 +0500

    google maps implemented

--- lib/screens/maps_screen.dart
+++ lib/screens/maps_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
@@ -8,10 +11,28 @@ class MapScreen extends StatefulWidget {
 }
 
 class _MapScreenState extends State<MapScreen> {
+  /*
+  AIzaSyCR5J14v_B_aD0VAC1spnT-Hhc7ymsPvqk
+  */
+
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(34.0151, 71.5249),
+    zoom: 14.4746,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Google Maps')),
+      body: GoogleMap(
+        mapType: MapType.satellite,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
     );
   }
 }
